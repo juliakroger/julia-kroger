e192edd1696d5e4bc2d3d8cb93c9e4908f223aea
Julia Kroger <julia@seedify.fund>
2023-09-11 19:21:09 -0300

    feat: add libs and config

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+
+      <div className="relative z-0"></div>
+    </div>
+  );
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const path = require("path");
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,7 +10,6 @@ export default defineConfig({
       stream: "stream-browserify",
       zlib: "browserify-zlib",
       util: "util",
-      web3: path.resolve(__dirname, "./node_modules/web3/dist/web3.min.js"),
     },
   },
   plugins: [react()],

940a27a672f18014c4b748e53bf69c0491a0702f
Julia Kroger <julia@seedify.fund>
2023-09-12 20:10:48 -0300

    feat: add starts component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import MyInfo from "@/components/MyInfo";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import StarsCanvas from "@/components/canvas/Stars";
 
 const App = () => {
   return (
@@ -10,9 +11,13 @@ const App = () => {
         <Navbar />
         <MyInfo />
       </div>
-      <div className="bg-primary flex flex-col items-center">
+      <div className="bg-primary flex flex-col items-center pb-20">
         <About />
+      </div>
+
+      <div className="bg-primary relative z-0">
         <Experience />
+        <StarsCanvas />
       </div>
     </div>
   );

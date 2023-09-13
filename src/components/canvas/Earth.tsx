2f1823d885b11bec35f124eafdd205abc3616ba9
Julia Kroger <julia@seedify.fund>
2023-09-13 16:59:38 -0300

    fix: import and file name

--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
+import CanvasLoader from "@/components/CanvasLoader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
@@ -15,7 +15,7 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
